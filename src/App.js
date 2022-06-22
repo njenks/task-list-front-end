113139f124717ebf2fb8ce36aeabf4dbad489d1b
Natascha <njenkns@gmail.com>
2022-06-22 14:10:57 -0700

    wave 3 in progress

--- src/App.js
+++ src/App.js
@@ -2,41 +2,67 @@ import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 import { useState } from 'react';
+import { useEffect } from 'react'; 
+import axios from 'axios'; 
 
 function App() {
-  const [tasks, setTasks] = useState([
-    {
-      id: 1,
-      title: 'Mow the lawn',
-      isComplete: false,
-    },
-    {
-      id: 2,
-      title: 'Cook Pasta',
-      isComplete: true,
-    },
-  ]);
+  const [tasks, setTasks] = useState([]); 
+
+  const URL = 'http://localhost:5000/tasks'; 
+
+  useEffect(() => { 
+    axios.get(URL)
+    .then((response) => { 
+      const updatedTasks = response.data.map((task)=> { 
+        return { 
+          id: task.id, 
+          title: task.title, 
+          isComplete: task.isComplete
+        };
+      });
+      setTasks(updatedTasks); 
+    })
+    .catch((err) => { 
+      console.log(err); 
+    });
+  }, []); 
+
 
   const flipComplete = (id) => {
-    const updatedTasks = [];
-    for (const task of tasks) {
-      if (task.id === id) {
-        task.isComplete = !task.isComplete;
-      }
-      updatedTasks.push(task);
-    }
-    setTasks(updatedTasks);
-  };
+    axios 
+      .patch(`${URL}/${id}/mark_complete`)
+      .then (() => { 
+        const updatedTasks = [];
+        for (const task of tasks) {
+          const newTask = {...task}; 
+          if (newTask.id === id) {
+            newTask.isComplete = !newTask.isComplete;
+          }
+          updatedTasks.push(newTask);
+        }
+        setTasks(updatedTasks);
+      })
+      .catch((err) => { 
+        console.log(err); 
+      }); 
+  }; 
 
   const deleteTask = (id) => {
-    const updatedTasks = [];
-    for (const task of tasks) {
-      if (task.id !== id) {
-        updatedTasks.push(task);
+    axios
+      .delete(`${URL}/${id}`)
+      .then(() => {
+        const updatedTasks = [];
+        for (const task of tasks) {
+        if (task.id !== id) {
+          updatedTasks.push(task);
+        }
       }
-    }
-    setTasks(updatedTasks);
-  };
+      setTasks(updatedTasks);
+    })
+    .catch((err) => { 
+      console.log(err);
+    }); 
+  }; 
 
   return (
     <div className="App">
--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -4,19 +4,21 @@ import Task from './Task.js';
 import './TaskList.css';
 
 const TaskList = (props) => {
-  const taskComponents = props.tasks.map((task) => {
-    return (
-      <Task
-        key={task.id}
-        id={task.id}
-        title={task.title}
-        isComplete={task.isComplete}
-        completeCallback={props.completeCallback}
-        deleteCallback={props.deleteCallback}
-      />
-    );
-  });
-  return <div>{taskComponents}</div>;
+  const getTaskListJSX = (props) => {
+    return props.tasks.map((task) => {
+      return (
+        <Task
+          key={task.id}
+          id={task.id}
+          title={task.title}
+          isComplete={task.isComplete}
+          completeCallback={props.completeCallback}
+          deleteCallback={props.deleteCallback}
+        />
+      );
+    });
+  };
+  return <ul className="tasks__list no-bullet">{getTaskListJSX(props)}</ul>;
 };
 
 TaskList.propTypes = {
